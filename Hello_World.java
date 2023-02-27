725435e4fc7bd1c66cb2b8ea456b32bf04f9e52d
Jit Chakraborty <110622733+jit-12345@users.noreply.github.com>
2023-02-27 21:05:29 +0530

    class added

---
+++ Hello_World.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Hello_World{
+  public static  void printer(){
+    System.out.println("jeet");
+  
+  }
+  
+  public static void main(String args[])
+  {
+    //print hello world.
+    System.out.println("Hello world !!!");
+     System.out.println("Hello world2 !!!");
+    printer();
+  }
+}
+
