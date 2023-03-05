1073854b9098490bbcc449396941ec418debd1f3
Jit Chakraborty <110622733+jit-12345@users.noreply.github.com>
2023-03-05 23:35:28 +0530

    sorting added

---
+++ Hello_World copy.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Hello_World{
+  public static  void printer(){
+    System.out.println("jeet");
+  
+  }
+  
+  public static void main(String args[])
+  {
+    //print hello world.
+    System.out.println("Hello world !!!");
+     System.out.println("Hello world2 !!!");
+    System.out.println("hello i am thuin !!!");
+    
+    printer();
+  }
+}
+
